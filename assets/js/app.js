111d3a7d8e73f1ae8ec4cedce2f28c69e321c8c5
Feli87 <felipe.traina.azul@gmail.com>
2020-11-26 00:26:35 -0300

    Add Modal Config

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,3 +17,22 @@ document.getElementById("btn").addEventListener("click", function(){
         });
     }
 }, false);
+
+
+/*
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!  Configuracion Modal Config  !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+*/
+// Get the modal
+var modalConfig = document.getElementById("modal-config");
+// Get the button that opens the modal
+var btnConfig = document.getElementById("btn-config");
+// Get the <span> element that closes the modal
+var modalClose = document.getElementsByClassName("close-config")[0];
+// When the user clicks on the button, open the modal
+btnConfig.onclick = ()=> modalConfig.style.display = "block"; 
+// When the user clicks on <span> (x), close the modal
+modalClose.onclick = ()=> modalConfig.style.display = "none";
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = event => event.target == modalConfig ? modalConfig.style.display = "none" : "";
